674b2e6ccd852194e7ba88c3f26011f39f385dd1
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-04-29 08:27:36 -0300

    cria estrutura de pastas e flag nokill

--- xcodegen.sh
+++ xcodegen.sh
@@ -1,4 +1,11 @@
-killall Xcode
+#!/bin/bash
+
+if [ "$1" == "nokill" ]; then
+    echo "Skipping killall"
+else
+    killall Xcode
+fi
+
 rm -rf PiraruCooks.xcodeproj 
 xcodegen
 open PiraruCooks.xcodeproj
