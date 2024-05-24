765f9a9947c04b273e00180474d7d6b24b8626d5
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-05-24 09:31:15 -0300

    ajusta detalhes visuais na busca e sacola

--- PiraruCooks/Components/Views/ListOfDishesView.swift
+++ PiraruCooks/Components/Views/ListOfDishesView.swift
@@ -22,49 +22,7 @@ struct ListOfDishesView: View {
                         selectedDish = dish
                         isHomePresented.toggle()
                     } label: {
-                        VStack {
-                            HStack {
-                                if dish.image != nil {
-                                    Image(uiImage: dish.image!)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: getWidth() * 0.25, height: getWidth() * 0.25)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                } else {
-                                    Shared.Images.emptyDish.swiftUIImage
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: getWidth() * 0.25, height: getWidth() * 0.25)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                }
-
-                                VStack(alignment: .leading) {
-                                    Text(dish.name)
-                                        .font(.body)
-                                        .fontWeight(.semibold)
-                                        .fixedSize(horizontal: false, vertical: true)
-                                    Spacer()
-                                    Text(dish.detailText)
-                                        .font(.subheadline)
-                                        .lineLimit(2)
-                                        .fixedSize(horizontal: false, vertical: true)
-                                    Spacer()
-                                    Text("R$ \(replaceDotWithComma(String(format: "%.2f", dish.price)))")
-                                        .font(.subheadline)
-                                }
-                                .multilineTextAlignment(.leading)
-                                .padding(.horizontal, 8)
-                                .frame(height: getWidth() * 0.25)
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .foregroundStyle(Shared.Colors.mediumGray.swiftUIColor)
-                                    .fontWeight(.semibold)
-                            }
-                            .padding(.vertical, 8)
-                            .foregroundStyle(.black)
-                            Divider()
-                                .padding(.vertical, 8)
-                        }
+                        DishCardView(viewModel: DishCardViewModel(dish: dish))
                     }
                 }
             }
