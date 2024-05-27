a60f2e7d211a3f5445676e9c7a9c2dae4f9796a2
Caio <caio_cz11@hotmail.com>
2024-05-27 11:42:45 -0300

    Adiciona tela de revisao de pedido

--- PiraruCooks/Components/Views/ReviewOrderView.swift
+++ PiraruCooks/Components/Views/ReviewOrderView.swift
@@ -7,9 +7,63 @@
 
 import SwiftUI
 
+/*
+struct PriceOption: Identifiable {
+    let id = UUID()
+    let title: String
+}
+*/
+
 struct ReviewOrderView: View {
+    @State private var selectedOption = "Crédito"
+    @State private var address: String = "R. Luverci Pereira De Souza, 1681\nCidade Universitária - Casa"
+    
+    let priceOptions = ["Crédito","Débito","Pix"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+                List{
+                    Section(header: Text("Entregar no endereço")) {
+                        HStack {
+                            Text(address)
+                                .padding(.horizontal)
+                                .font(.subheadline)
+                            
+                            Button(action: {})
+                            {
+                                Text("Trocar")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+                    Picker("Opções de pagamento", selection: $selectedOption) {
+                        ForEach(priceOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.inline)
+
+                }
+            HStack {
+                Text("Total com a entrega")
+                    .font(.headline)
+                Spacer()
+                Text("R$ 22,63 / 1 item")
+                    .font(.headline)
+                    .foregroundColor(.green)
+            }
+                .padding(.horizontal)
+                Button(action: {}) {
+                    Text("Continuar")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding()
+                .navigationTitle("Sacola")
+                .navigationBarTitleDisplayMode(.inline)
+            }
     }
 }
 
